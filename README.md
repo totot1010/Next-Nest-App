11d2ee87cfc0e6be4c212e9b32aa643b3f65f0d3
yutocreate <1otuy26@gmail.com>
2022-10-17 00:47:46 +0900

    postgres登録

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Next-Nest-TodoApp
+
+
+## posgtresに接続
+```
+psql -h localhost -U user -d db
+```
---
+++ backend/prisma/migrations/20221016135050_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
